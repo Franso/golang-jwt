fa402b39a98aa735612a47e4b599913cb5893219
Franso <francismuasya@gmail.com>
2022-07-25 12:31:06 +0300

    initial setup of auth and user route handlers

--- routes/authRouter.go
+++ routes/authRouter.go
@@ -1,1 +1,13 @@
 package routes
+
+import (
+	controller "github.com/Franso/golang-jwt/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func AuthRoutes(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("users/signup", controller.Signup())
+	incomingRoutes.POST("users/login", controller.Login())
+
+}
