278b2dddbfa31b0b1efbfe2fa7fb409870b76494
Franso <francismuasya@gmail.com>
2022-07-26 14:35:02 +0300

    create validate token function

--- middleware/authMiddleware.go
+++ middleware/authMiddleware.go
@@ -1,1 +1,25 @@
 package middleware
+
+import (
+	"fmt"
+	"net/http"
+
+	helper "github.com/Franso/golang-jwt/helpers"
+	"github.com/gin-gonic/gin"
+)
+
+func Authenticate() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		clientToken := c.Request.Header.Get("token")
+
+		if clientToken == "" {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": fmt.Sprintf("No authorization headers provided")})
+			c.Abort()
+			return
+		}
+
+		claims, err := helper.ValidateToken(clientToken)
+	}
+}
+
+func ValidateToken() {}
