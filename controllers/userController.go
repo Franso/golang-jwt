1440d5b61e8f76c592ec1f3461e778b25a545279
Franso <francismuasya@gmail.com>
2022-07-26 09:56:10 +0300

    create GetUser function Logic

--- controllers/userController.go
+++ controllers/userController.go
@@ -1,12 +1,16 @@
 package controllers
 
 import (
+	"context"
 	"net/http"
+	"time"
 
 	"github.com/Franso/golang-jwt/database"
 	helper "github.com/Franso/golang-jwt/helpers"
+	"github.com/Franso/golang-jwt/models"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
@@ -31,5 +35,20 @@ func GetUser() gin.HandlerFunc {
 			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 			return
 		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		var user models.User
+		err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+
+		defer cancel()
+
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+
+		c.JSON(http.StatusOK, user)
+
 	}
 }
